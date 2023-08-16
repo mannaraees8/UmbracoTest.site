9302d99c4cd01fb10abd0252d67946ca2961a609
mannaraees <79034666+mannaraees@users.noreply.github.com>
2023-08-16 15:14:30 +0400

    initial commit

---
+++ UmbracoTest.site/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,43 @@
+﻿using Umbraco;
+using System.Web;
+using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco.Cms.Core.Web;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.BackOffice.ActionResults;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using UmbracoTest.site.umbraco.models;
+using System.Net.Mail;
+
+namespace UmbracoTest.site.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public ContactSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        public ActionResult SubmitForm(ContactModel contactModel)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(contactModel);
+                TempData["isEmailSent"] = true;
+            }
+            return Redirect("/");
+        }
+
+        private void SendEmail(ContactModel contactModel)
+        {
+            MailMessage message = new MailMessage(contactModel.Email, "mannaraees8@gmail.com");
+            message.Subject = $"Enquiry from {contactModel.FullName}";
+            message.Body = $"<p>Full Name : {contactModel.FullName}<p><p>Email : {contactModel.Email}<p><p>Phone : {contactModel.Phone}<p><p>Message : {contactModel.Message}<p>";
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("127.0.0.1",25);
+            client.Send(message);
+        }
+    }
+}
---
+++ UmbracoTest.site/umbraco/models/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoTest.site.umbraco.models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string? FullName { get; set; }
+        [EmailAddress]
+        [Required]
+        public string? Email { get; set; }
+        [Phone]
+        [Required]
+        public string? Phone { get; set; }
+        [Required]
+        public string? Message { get; set; }
+    }
+}
